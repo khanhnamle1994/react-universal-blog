be14374b8eb51377331cdb490a09c78467b733dc
James Le <le_j6@denison.edu>
2017-07-12 13:53:23 -0700

    Server Side Rendering

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 // config.js
 export default {
   site: {
-    title: 'James Le Blog'
+    title: 'React Universal Blog'
   },
   bucket: {
-    slug: 'james-le-blog',
-    media_url: 'https://api.cosmicjs.com/v1/james-le-blog'
-  }
+    slug: process.env.COSMIC_BUCKET || 'react-universal-blog',
+    media_url: 'https://cosmicjs.com/uploads',
+    read_key: process.env.COSMIC_READ_KEY || '',
+    write_key: process.env.COSMIC_WRITE_KEY || ''
+  },
 }

1e589a42f516e353336b98c9a4e9582ba3d8762e
James Le <le_j6@denison.edu>
2017-07-12 13:24:43 -0700

    All Page and Partial Components Added

--- components/Pages/Work.js
+++ components/Pages/Work.js
@@ -1,17 +1,17 @@
-// Blog.js
+// Work.js
 import React, { Component } from 'react'
 import _ from 'lodash'
 import config from '../../config'
 
 // Components
 import Header from '../Partials/Header'
-import BlogList from '../Partials/BlogList'
-import BlogSingle from '../Partials/BlogSingle'
+import WorkList from '../Partials/WorkList'
+import WorkSingle from '../Partials/WorkSingle'
 
 // Dispatcher
 import AppDispatcher from '../../dispatcher/AppDispatcher'
 
-export default class Blog extends Component {
+export default class Work extends Component {
 
   componentWillMount(){
     this.getPageData()
@@ -25,12 +25,12 @@ export default class Blog extends Component {
   getPageData(){
     AppDispatcher.dispatch({
       action: 'get-page-data',
-      page_slug: 'blog',
+      page_slug: 'work',
       post_slug: this.props.params.slug
     })
   }
 
-  getMoreArticles(){
+  getMoreWorkItems(){
     AppDispatcher.dispatch({
       action: 'get-more-items'
     })
@@ -45,28 +45,29 @@ export default class Blog extends Component {
 
     if(!this.props.params.slug){
 
-      main_content =
+      main_content = <WorkList getMoreWorkItems={ this.getMoreWorkItems } data={ data }/>
 
     } else {
-
-      const articles = data.articles
-
+      
+      const work_items = data.work_items
+      
       // Get current page slug
       const slug = this.props.params.slug
-      const articles_object = _.indexBy(articles, 'slug')
-      const article = articles_object[slug]
-      main_content =
+      const work_items_object = _.indexBy(work_items, 'slug')
+      const work_item = work_items_object[slug]
+      
+      main_content = <WorkSingle data={ data } work_item={ work_item }/>
 
     }
 
     return (
       <div>
         <Header data={ data }/>
-        <div id="main-content" className="container">
-          <div className="row">
-            <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-            { main_content }
-            </div>
+          <div id="main-content" className="container">
+            <div className="row">
+              <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+              { main_content }
+              </div>
           </div>
         </div>
       </div>
--- components/Partials/WorkList.js
+++ components/Partials/WorkList.js
@@ -1,9 +1,12 @@
-// BlogList.js
+// WorkList.js
 import React, { Component } from 'react'
 import _ from 'lodash'
 import { Link } from 'react-router'
 
-export default class BlogList extends Component {
+// Dispatcher
+import AppDispatcher from '../../dispatcher/AppDispatcher'
+
+export default class WorkList extends Component {
 
   scrollTop(){
     $('html, body').animate({
@@ -12,38 +15,38 @@ export default class BlogList extends Component {
   }
 
   render(){
-
+    
     let data = this.props.data
     let item_num = data.item_num
-    let articles = data.articles
+    let work_items = data.work_items
 
     let load_more
-    let show_more_text = 'Show More Articles'
+    let show_more_text = 'Show More Work'
 
     if(data.loading){
       show_more_text = 'Loading...'
     }
 
-    if(articles && item_num <= articles.length){
+    if(work_items && item_num <= work_items.length){
       load_more = (
         <div>
-          <button className="btn btn-default center-block" onClick={ this.props.getMoreArticles.bind(this) }>
+          <button className="btn btn-default center-block" onClick={ this.props.getMoreWorkItems.bind(this) }>
             { show_more_text }
           </button>
         </div>
       )
     }
 
-    articles = _.take(articles, item_num)
-
-    let articles_html = articles.map(( article ) => {
-      let date_obj = new Date(article.created)
+    work_items = _.take(work_items, item_num)
+    
+    let articles_html = work_items.map(( work_item ) => {
+      let date_obj = new Date(work_item.created)
       let created = (date_obj.getMonth()+1) + '/' + date_obj.getDate() + '/' + date_obj.getFullYear()
       return (
-        <div key={ 'key-' + article.slug }>
+        <div key={ 'key-' + work_item.slug }>
           <div className="post-preview">
             <h2 className="post-title pointer">
-              <Link to={ '/blog/' + article.slug } onClick={ this.scrollTop }>{ article.title }</Link>
+              <Link to={ '/work/' + work_item.slug } onClick={ this.scrollTop }>{ work_item.title }</Link>
             </h2>
             <p className="post-meta">Posted by <a href="https://cosmicjs.com" target="_blank">Cosmic JS</a> on { created }</p>
           </div>

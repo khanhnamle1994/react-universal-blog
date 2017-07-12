fc4852acff3a13917d1540a4a25c001a3395b1a2
James Le <le_j6@denison.edu>
2017-07-12 13:39:14 -0700

    Configure Cosmic JS CMS

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+export default {
+  site: {
+    title: 'James Le Blog'
+  },
+  bucket: {
+    slug: 'james-le-blog',
+    media_url: 'https://api.cosmicjs.com/v1/james-le-blog'
+  }
+}
